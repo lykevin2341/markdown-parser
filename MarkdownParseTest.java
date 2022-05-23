28a0c56aa6c0250a8ca84c0c4b90afb6abb16714
lykevin2341 <k9ly@ucsd.edu>
2022-05-22 23:30:06 -0700

    Finished all of lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,89 +9,89 @@ import org.junit.*;
 
 public class MarkdownParseTest {
 
-	@Test
-	public void addition() {
-		assertEquals(2, 1+1);
-	}
+	// @Test
+	// public void addition() {
+	// 	assertEquals(2, 1+1);
+	// }
 	
-	@Test
-	public void testMarkdownFileOne() {
-		ArrayList<String> toReturn = new ArrayList<>();
-		toReturn.add("some-thing.html");
+	// @Test
+	// public void testMarkdownFileOne() {
+	// 	ArrayList<String> toReturn = new ArrayList<>();
+	// 	toReturn.add("some-thing.html");
 		
-		try {
-			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file.md"))));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	// 	try {
+	// 		assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file.md"))));
+	// 	} catch (IOException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// }
 	
-	@Test
-	public void testMarkdownFileTwo() {
-		ArrayList<String> toReturn = new ArrayList<>();
-		toReturn.add("test.html");
+	// @Test
+	// public void testMarkdownFileTwo() {
+	// 	ArrayList<String> toReturn = new ArrayList<>();
+	// 	toReturn.add("test.html");
 		
-		try {
-			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file2.md"))));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	// 	try {
+	// 		assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file2.md"))));
+	// 	} catch (IOException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// }
 	
-	@Test
-	public void testMarkdownFileThree() {
-		ArrayList<String> toReturn = new ArrayList<>();
-		toReturn.add("https://something.com");
-		toReturn.add("some-thing.html");
-		toReturn.add("testing.html");
+	// @Test
+	// public void testMarkdownFileThree() {
+	// 	ArrayList<String> toReturn = new ArrayList<>();
+	// 	toReturn.add("https://something.com");
+	// 	toReturn.add("some-thing.html");
+	// 	toReturn.add("testing.html");
 		
-		try {
-			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file3.md"))));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	// 	try {
+	// 		assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file3.md"))));
+	// 	} catch (IOException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// }
 
-	@Test
-	public void testMarkdownFileFour(){
-		ArrayList<String> toReturn = new ArrayList<>();
-		try{
-			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file4.md"))));
-		} catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+	// @Test
+	// public void testMarkdownFileFour(){
+	// 	ArrayList<String> toReturn = new ArrayList<>();
+	// 	try{
+	// 		assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file4.md"))));
+	// 	} catch(IOException e){
+	// 		e.printStackTrace();
+	// 	}
+	// }
 	
-	@Test
-	public void testMarkdownFileFive() {
-		ArrayList<String> toReturn = new ArrayList<>();
-		toReturn.add("test-website");
-		toReturn.add("second-test-website");
-		try {
-			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file5.md"))));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	// @Test
+	// public void testMarkdownFileFive() {
+	// 	ArrayList<String> toReturn = new ArrayList<>();
+	// 	toReturn.add("test-website");
+	// 	toReturn.add("second-test-website");
+	// 	try {
+	// 		assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file5.md"))));
+	// 	} catch (IOException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// }
 
-	@Test 
-	public void testMarkdownFileSix() {
-		ArrayList<String> toReturn = new ArrayList<>();
-		toReturn.add("www.twitter.com");
-		toReturn.add("www.google.com");
-		try {
-			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file6.md"))));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	// @Test 
+	// public void testMarkdownFileSix() {
+	// 	ArrayList<String> toReturn = new ArrayList<>();
+	// 	toReturn.add("www.twitter.com");
+	// 	toReturn.add("www.google.com");
+	// 	try {
+	// 		assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of(System.getProperty("user.dir") + File.separator + "test-file6.md"))));
+	// 	} catch (IOException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// }
 	
 	@Test
 	public void testLab4Snippet1() throws IOException{
 		Path pathOfSnip = Path.of("./lab4snippet1.md");
 		String testerFile = Files.readString(pathOfSnip);
 		ArrayList<String> expectedList = new ArrayList<>();
-		expectedList.add("url.com"));
+		expectedList.add("url.com");
 		expectedList.add("`google.com");
 		expectedList.add("google.com");
 		expectedList.add("ucsd.edu");
